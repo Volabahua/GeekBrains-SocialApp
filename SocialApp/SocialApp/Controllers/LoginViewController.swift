0e82259d56b54c35ac7416a274f13992a09e321e
Vlabakhua <volabahua@gmail.com>
2020-09-29 02:06:14 +0300

    # - добавил LoginViewController

--- SocialApp/SocialApp/Controllers/LoginViewController.swift
+++ SocialApp/SocialApp/Controllers/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
    //Объявляем скроллер для вложенного контента
     @IBOutlet weak var scroller: UIScrollView!
     // Когда клавиатура появляется
