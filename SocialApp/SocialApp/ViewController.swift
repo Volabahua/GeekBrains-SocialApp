a7a53a79e32f53144e876df220d57a63ce628a04
Vlabakhua <volabahua@gmail.com>
2020-09-28 21:40:48 +0300

    # - работает проверка логина и пароля добавлены экраны

--- SocialApp/SocialApp/ViewController.swift
+++ SocialApp/SocialApp/ViewController.swift
@@ -8,25 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
-   
+   //Объявляем скроллер для вложенного контента
     @IBOutlet weak var scroller: UIScrollView!
-    
     // Когда клавиатура появляется
     @objc func keyboardWasShown(notification: Notification) {
-        
         // Получаем размер клавиатуры
         let info = notification.userInfo! as NSDictionary
         let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
         // Параметр kbSize.height делится на 2 для лучшего отображения на маленьких экранах
         let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height/2, right: 0.0)
-        
         // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
         self.scroller?.contentInset = contentInsets
         scroller?.scrollIndicatorInsets = contentInsets
     }
-    
     //Когда клавиатура исчезает
     @objc func keyboardWillBeHidden(notification: Notification) {
         // Устанавливаем отступ внизу UIScrollView, равный 0
@@ -36,14 +30,12 @@ class ViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
             super.viewWillDisappear(animated)
-            
             NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
             NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         }
     
     override func viewWillAppear(_ animated: Bool) {
             super.viewWillAppear(animated)
-            
             // Подписываемся на два уведомления: одно приходит при появлении клавиатуры
             NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
             // Второе — когда она пропадает
@@ -54,42 +46,58 @@ class ViewController: UIViewController {
             self.scroller?.endEditing(true)
         }
 
-    
+    // Текстовое поле логина
     @IBOutlet weak var loginInput: UITextField!
+    // Текстовое поле пароля
     @IBOutlet weak var passwordInput: UITextField!
    
     
     override func viewDidLoad() {
             super.viewDidLoad()
-         
             // Жест нажатия
             let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
             // Присваиваем его UIScrollVIew
             scroller?.addGestureRecognizer(hideKeyboardGesture)
         }
-
-   
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//    }
-
-    @IBAction func loginButtomPressed(_ sender: Any){
+    
+    
+    // Переход к приложению при правильно указанном логине и пароле
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+            // Проверяем данные
+            let checkResult = checkUserData()
+            // Если данные не верны, покажем ошибку
+            if !checkResult {
+                showLoginError()
+            }
+            // Вернем результат
+            return checkResult
+        }
         
-        // Получаем текст логина
-        let login = loginInput.text!
-        // Получаем текст-пароль
-        let password = passwordInput.text!
+    // Сравниваем введенные данные с теми что указаны тут
+        func checkUserData() -> Bool {
+            guard let login = loginInput.text,
+                let password = passwordInput.text else { return false }
+            if login == "admin" && password == "123456" {
+                return true
+            } else {
+                return false
+            }
+        }
         
-        // Проверяем, верны ли они
-        if login == "admin" && password == "123456" {
-            print("успешная авторизация")
-        } else {
-            print("неуспешная авторизация")
+    // Показываем уведомление при неправильном вводе пароля
+        func showLoginError() {
+            // Создаем контроллер
+            let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
+            // Создаем кнопку для UIAlertController
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            // Добавляем кнопку на UIAlertController
+            alter.addAction(action)
+            // Показываем UIAlertController
+            present(alter, animated: true, completion: nil)
         }
 
-        
+    // Нажатие на кнопку "Вход"
+    @IBAction func loginButtomPressed(_ sender: Any){
     }
     
 }
-
